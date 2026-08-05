dec5bd86771dc6873ce382777956409a3e2dcd09
Muslim A.Nadheer <Mu.Ashoor@gmail.com>
2026-08-05 09:59:07 +0300

    Remove get_db function from session.py
    Removed the get_db function which provided a database session.

--- app/database/session.py
+++ app/database/session.py
@@ -9,9 +9,3 @@ engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
